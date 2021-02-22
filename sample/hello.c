f5d3f4f832273a402203ed23a0ec91c963a2d6bc
Uuuuuuho <dbgh9129@gmail.com>
2021-02-22 21:47:24 +0900

    isa_test.S update

---
+++ sample/hello.c
@@ -0,0 +1,10 @@
+// #include <stdio.h>
+int main(void) 
+{ 
+	// printf("Hello world!\n");
+	int a = 1;
+	int b = 2;
+	int c = a + b;
+	return 0;
+}
+
---
+++ sample/main.c
@@ -0,0 +1,5 @@
+// #include "riscv_arithmetic_basis_test_0.S"
+void main()
+{
+	
+}
